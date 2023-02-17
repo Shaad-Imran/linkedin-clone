4a798913536e9550194b70c1e4f664b96bcdd7ba
Imran <imranmansuri038@gmail.com>
2023-02-17 21:45:23 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
